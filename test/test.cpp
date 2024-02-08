7c13389e26ba426b0ae4cf948e118130dd11284f
Michael Dombrowski <mdombro@amazon.com>
2024-02-08 14:02:08 -0500

    Add first basic test

--- test/test.cpp
+++ test/test.cpp
@@ -1,11 +1,13 @@
 #include <catch2/catch_test_macros.hpp>
 #include <memory>
 #include <filesystem>
+#include <string>
 #include "kv/kv.hpp"
 #include "filesystem/posixFileSystem.hpp"
 
 using namespace aws::gg;
 using namespace aws::gg::kv;
+using namespace std::string_literals;
 
 SCENARIO("I can create a KV map", "[kv]") {
     GIVEN("I create an empty KV map") {
@@ -16,5 +18,20 @@ SCENARIO("I can create a KV map", "[kv]") {
             .compact_after = 0,
         });
         REQUIRE(kv_or);
+
+        auto kv = std::move(kv_or.val());
+
+        auto key = "key"s;
+        auto value = "value"s;
+        WHEN("I add a value") {
+            auto e = kv->put(key, BorrowedSlice{value});
+            REQUIRE(e.code == KVErrorCodes::NoError);
+
+            THEN("I can get the value back") {
+                auto v_or = kv->get(key);
+                REQUIRE(v_or);
+                REQUIRE(v_or.val().string() == value);
+            }
+        }
     }
 }
